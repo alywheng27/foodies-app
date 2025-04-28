9c1a7f85a9bbb124fc9d3cab9979904293aac442
Aly Mama <aly070296@gmail.com>
2025-04-28 12:54:41 +0800

    105. React Server Components vs Client Components - When To Use What

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import styles from './page.module.css'
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={styles.header}>
         <div className={styles.slideshow}>
-
+          <ImageSlideshow />
         </div>
         <div className={styles.hero}>
           <h1>NextLevel Food for Nextlevel Foodies</h1>
